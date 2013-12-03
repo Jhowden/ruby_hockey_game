ce108939718de74bcfca4efddb48ed77b624d383
Jonathan Howden <jonathanhowden@gmail.com>
2013-12-02 21:08:09 -0600

    Created an empty array for the user's team

---
+++ team_setup.rb
@@ -0,0 +1,10 @@
+class TeamSetup
+
+	attr_accessor :team
+
+	def initialize
+		@team = []
+	end
+
+	
+end
--- team_setup_spec.rb
+++ team_setup_spec.rb
@@ -1,1 +1,10 @@
 require_relative 'team_setup'
+
+describe TeamSetup do
+  context '#initialize' do
+  	it 'instantiates an empty array' do
+  		setup = TeamSetup.new
+  		expect(setup.team).to eq([])
+  	end
+  end
+end
