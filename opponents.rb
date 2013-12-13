1b24e9e2c631603c4e2e58132b532dff22dd322d
Jonathan Howden <jonathanhowden@gmail.com>
2013-12-13 10:12:37 -0600

    Added a second opponent

--- opponents.rb
+++ opponents.rb
@@ -5,4 +5,11 @@ module Opponent
 													 left_defensemen: LeftDefensemen.new({name:"Sweet Dee", shooting_power:62, puck_control:63, body_check:62, shot_block:63, stick_check:62, balance:63, puck:[]}), 
 													 right_defensemen: RightDefensemen.new({name:"Frank", shooting_power:63, puck_control:62, body_check:63, shot_block:62, stick_check:63, balance:62, puck:[]}), 
 													 goalie: Goalie.new({name:"The Waitress", reflexes:63, rebound_control:62, puck:[]})})
+
+  TURTLES = Team.new("Turles", {center: Center.new({name:"Leonardo", shooting_power:75, puck_control:75, body_check:75, shot_block:75, stick_check:75, balance:75, puck:[], faceoff:70}), 
+													 left_wing: LeftWing.new({name:"Michaelangelo", shooting_power:68, puck_control:70, body_check:68, shot_block:70, stick_check:68, balance:70, puck:[]}), 
+													 right_wing: RightWing.new({name:"Dennis", shooting_power:70, puck_control:68, body_check:70, shot_block:68, stick_check:70, balance:68, puck:[]}), 
+													 left_defensemen: LeftDefensemen.new({name:"Sweet Dee", shooting_power:68, puck_control:70, body_check:68, shot_block:70, stick_check:68, balance:70, puck:[]}), 
+													 right_defensemen: RightDefensemen.new({name:"Frank", shooting_power:70, puck_control:68, body_check:70, shot_block:68, stick_check:70, balance:68, puck:[]}), 
+													 goalie: Goalie.new({name:"The Waitress", reflexes:70, rebound_control:68, puck:[]})})
 end
