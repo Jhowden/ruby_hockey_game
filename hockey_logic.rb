4430725643dba812c45c09d7b6463d7c3392860c
Jonathan Howden <jonathanhowden@gmail.com>
2013-12-13 10:11:28 -0600

    Added to hockey logic to allow goals

--- hockey_logic.rb
+++ hockey_logic.rb
@@ -1,7 +1,7 @@
 module Action
 
   def self.check(offensive_player, defensive_player)
-    if defensive_player.check > offensive_player.balance
+    if defensive_player.body_check > offensive_player.balance
       defensive_player.puck << offensive_player.puck.pop
     end
   end
@@ -12,22 +12,22 @@ module Action
     end
   end
 
-  def self.block_shot(offensive_player, defensive_player, goalie)
+  def self.block_shot(offensive_player, defensive_player, defensive_goalie, defensive_net)
     probability = defensive_player.shot_block * 0.4
     if rand(100) < probability
       defensive_player.puck << offensive_player.puck.pop
     else
-      self.goal?(offensive_player, goalie)
+      self.goal?(offensive_player, defensive_goalie, defensive_net)
     end
   end
 
-  def self.goal?(offensive_player, goalie)
-    if offensive_player.shooting_power > goalie.reflexes
-      offensive_player.puck.pop
-      "goal"
+  def self.goal?(offensive_player, defensive_goalie, defensive_net)
+    if offensive_player.shooting_power > defensive_goalie.reflexes
+      defensive_net.goal_net << offensive_player.puck.pop
+      puts "goal"
     else
-      goalie.puck << offensive_player.puck.pop
-      "faceoff"
+      defensive_goalie.puck << offensive_player.puck.pop
+      puts "faceoff"
     end
   end
 
--- hockey_logic_spec.rb
+++ hockey_logic_spec.rb
@@ -4,7 +4,7 @@ describe Action do
   context "#check" do
     it 'returns an offensive player with no puck when checked' do
       offensive_player = double(balance: 50, puck: ["puck"])
-      defensive_player = double(check: 60, puck: [])
+      defensive_player = double(body_check: 60, puck: [])
 
       Action.check(offensive_player, defensive_player)
       expect(offensive_player.puck).to eq([])
@@ -13,7 +13,7 @@ describe Action do
 
     it 'returns an offensive player with a puck when checked' do
       offensive_player = double(balance: 70, puck: ["puck"])
-      defensive_player = double(check: 60, puck: [])
+      defensive_player = double(body_check: 60, puck: [])
 
       Action.check(offensive_player, defensive_player)
       expect(offensive_player.puck).to eq(["puck"])
@@ -45,12 +45,13 @@ describe Action do
     it 'returns a block shot at least once out of 10 shots for a defender with perfect shot blocking' do
       offensive_player = double(puck: ["puck"], shooting_power: 70)
       defensive_player = double(shot_block: 100, puck: [])
-      goalie = double(puck: [], reflexes: 65)
+      defensive_goalie = double(puck: [], reflexes: 65)
+      defensive_net = double(goal_net: [])
 
       blocks = []
       10.times do 
-        if Action.block_shot(offensive_player, defensive_player, goalie)
-          blocks << Action.block_shot(offensive_player, defensive_player, goalie)
+        if Action.block_shot(offensive_player, defensive_player, defensive_goalie, defensive_net)
+          blocks << Action.block_shot(offensive_player, defensive_player, defensive_goalie, defensive_net)
         end
       end
 
@@ -60,39 +61,47 @@ describe Action do
     it 'returns a goal when a shot is not blocked and an offensive player\'s shot power is greater than a goalie\'s reflexes' do
       offensive_player = double(puck: ["puck"], shooting_power: 70)
       defensive_player = double(shot_block: 0, puck: [])
-      goalie = double(puck: [], reflexes: 65)
+      defensive_goalie = double(puck: [], reflexes: 65)
+      defensive_net = double(goal_net: [])
 
-      expect(Action.block_shot(offensive_player, defensive_player, goalie)).to eq("goal")
+      Action.block_shot(offensive_player, defensive_player, defensive_goalie, defensive_net)
       expect(offensive_player.puck).to eq([]) 
+      expect(defensive_net.goal_net).to eq(["puck"])
     end
 
     it 'returns a faceoff when a shot is not blocked and an offensive player\'s shot power is less than a goalie\'s reflexes' do
       offensive_player = double(puck: ["puck"], shooting_power: 65)
       defensive_player = double(shot_block: 0, puck: [])
-      goalie = double(puck: [], reflexes: 65)
+      defensive_goalie = double(puck: [], reflexes: 65)
+      defensive_net = double(goal_net: [])
 
-      expect(Action.block_shot(offensive_player, defensive_player, goalie)).to eq("faceoff")
-      expect(goalie.puck).to eq(["puck"])
+      Action.block_shot(offensive_player, defensive_player, defensive_goalie, defensive_net)
+      expect(defensive_goalie.puck).to eq(["puck"])
       expect(offensive_player.puck).to eq([])
+      expect(defensive_net.goal_net).to eq([])
     end
   end
 
   context "#goal" do
     it 'returns goal when an offensive player\'s shot power is greater than a goalie\'s reflexes' do
       offensive_player = double(puck: ["puck"], shooting_power: 70)
-      goalie = double(puck: [], reflexes: 65)
+      defensive_goalie = double(puck: [], reflexes: 65)
+      defensive_net = double(goal_net: [])
 
-      expect(Action.goal?(offensive_player, goalie)).to eq("goal")
+      Action.goal?(offensive_player, defensive_goalie, defensive_net)
       expect(offensive_player.puck).to eq([])
+      expect(defensive_net.goal_net).to eq(["puck"])
     end
 
     it 'returns a faceoff when an offensive player\'s shot power is less than a goalie\'s reflexes' do
       offensive_player = double(puck: ["puck"], shooting_power: 60)
-      goalie = double(puck: [], reflexes: 65)
+      defensive_goalie = double(puck: [], reflexes: 65)
+      defensive_net = double(goal_net: [])
 
-      expect(Action.goal?(offensive_player, goalie)).to eq("faceoff")
-      expect(goalie.puck).to eq(["puck"])
+      Action.goal?(offensive_player, defensive_goalie, defensive_net)
+      expect(defensive_goalie.puck).to eq(["puck"])
       expect(offensive_player.puck).to eq([])
+      expect(defensive_net.goal_net).to eq([])
     end
   end
 
